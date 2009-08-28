3edeca1f9d4b4c04cd068bb127db8afc095f14c1
Mauricio Collares <mauricioc@gmail.com>
2009-08-28 01:10:04 -0300

    Ultimas alteracoes no heap... Juro que nao mexo mais nele\!

--- structures/heap.cpp
+++ structures/heap.cpp
@@ -5,7 +5,7 @@ struct heap
     int heap[MAXV][2], v2n[MAXV];
     int size;
 
-    void init(int sz)
+    void init(int sz) __attribute__((always_inline))
     {
         memset(v2n, -1, sizeof(int) * sz);
         size = 0;
@@ -22,7 +22,7 @@ struct heap
     {
         swap(v2n[heap[a][1]], v2n[heap[b][1]]);
         swap(heap[a][0], heap[b][0]);
-	swap(heap[a][1], heap[b][1]);
+        swap(heap[a][1], heap[b][1]);
     }
 
     int extract_min()
@@ -32,20 +32,22 @@ struct heap
 
         int cur_pos = 0, next = 1;
         while(next < size) {
-            if(next + 1 < size && heap[next][0] > heap[next + 1][0])
-                next++;
+            if(heap[next][0] > heap[next - 1][0])
+                next--;
             if(heap[next][0] >= heap[cur_pos][0])
                 break;
 
             s(next, cur_pos);
             cur_pos = next;
-	    next = 2*cur_pos + 1;
+            next = 2*cur_pos + 2;
         }
+        if(next == size && heap[next - 1][0] < heap[cur_pos][0])
+            s(next - 1, cur_pos);
 
         return ret;
     }
 
-    int decrease_key(int vertex, int new_value)
+    void decrease_key(int vertex, int new_value) __attribute__((always_inline))
     {
         if(v2n[vertex] == -1)
         {
