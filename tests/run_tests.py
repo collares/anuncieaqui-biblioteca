597c2a1dfad5abbc679694e04dad5cb3b18321c6
Mauricio Collares <mauricioc@gmail.com>
2009-08-28 02:06:31 -0300

    Adicionando um script python tosco pra rodar os testes

---
+++ tests/run_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys, subprocess, os;
+
+testlist = file('test_list', 'r');
+
+for test in testlist:
+  test = test.rstrip('\n');
+  sys.stdout.write('Executando teste {0}... '.format(test));
+  sys.stdout.flush();
+
+  p = subprocess.Popen('./' + test + ' < ' + test + '.in > ' + test + '.out', shell=True);
+  os.waitpid(p.pid, 0);
+
+  if subprocess.call(['diff', test + '.out', test + '.sol']):
+    print 'Erro no teste do programa {0}!'.format(test);
+    break;
+  print 'OK!'
