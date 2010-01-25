379e15bd60229f4ab585a1f64fb49599b4e86af9
Mauricio Collares <mauricioc@gmail.com>
2010-01-25 16:41:59 -0200

    Big bug in the convex hull implementation

--- geometry/hull.cpp
+++ geometry/hull.cpp
@@ -9,10 +9,14 @@ vector<pt> hull(vector<pt> pts) {
     if(pts.size() <= 1) return pts;
     vector<pt> ret;
 
-    pivot = pts[0];
+    int mini = 0;
     for(int i = 1; i < pts.size(); i++)
-	pivot = min(pivot, pts[i]);
-    sort(pts.begin(), pts.end(), hull_comp);
+        if(pts[i] < pts[mini])
+	    mini = i;
+    
+    pivot = pts[mini];
+    swap(pts[0], pts[mini]);
+    sort(pts.begin() + 1, pts.end(), hull_comp);
 
     ret.push_back(pts[0]); 
     ret.push_back(pts[1]);
@@ -21,7 +25,7 @@ vector<pt> hull(vector<pt> pts) {
     for(int i = 2; i < pts.size(); i++) {
 	while(sz >= 2 && ccw(ret[sz-2], ret[sz-1], pts[i]) <= 0)
 	    ret.pop_back(), sz--;
-	ret.push_back(pts[i]);
+	ret.push_back(pts[i]), sz++;
     }
     
     return ret;
