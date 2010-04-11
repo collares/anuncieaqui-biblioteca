2a08cfb44e7dccd82dc7543c7186369fcb357e69
Mauricio Collares <mauricioc@gmail.com>
2010-04-10 22:10:48 -0300

    Simple optimization

--- math/sieve.cpp
+++ math/sieve.cpp
@@ -5,7 +5,7 @@ unsigned short notprime[MAX];
 vector<int> primes;
 
 void sieve() {
-    unsigned mod[16][16], di[16][16];
+    unsigned mod[16][16], di[16][16], num;
     for(int i = 0; i < 16; i++)
         for(int j = 0; j < 16; j++) {
             di[i][j] = (w[i]*w[j])/60;
@@ -18,12 +18,12 @@ void sieve() {
     for(unsigned i = 0; i < MAX; i++)
         for(int j = (i==0); j < 16; j++) {
             if(notprime[i] & (1<<j)) continue;
-            primes.push_back(60*i + w[j]);
+            primes.push_back(num = 60*i + w[j]);
 
             if(i > MAX_S) continue;
             for(unsigned k = i, done = false; !done; k++)
                 for(int l = 0; l < 16 && !done; l++) {
-                    unsigned mult = 60*i*k + i*w[l] + k*w[j] + di[j][l];
+                    unsigned mult = k*num + i*w[l] + di[j][l];
                     if(mult >= MAX) done = true;
                     else notprime[mult] |= 1<<mod[j][l];
                 }
