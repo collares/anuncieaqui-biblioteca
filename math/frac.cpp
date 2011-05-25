932055a107a6766afabd5c29a584fead5c097eb8
Mauricio Collares Neto <mauricioc@gmail.com>
2011-05-24 22:34:50 -0300

    Cleanup da classe fracao

--- math/frac.cpp
+++ math/frac.cpp
@@ -2,8 +2,7 @@ struct frac {
     long long num, den;
 
     frac() : num(0), den(1) { };
-    frac(long long num, long long den) { set_val(num, den); }
-    frac(long long num) : num(num), den(1) { };
+    frac(long long num, long long den = 1) { set_val(num, den); }
 
     void set_val(long long _num, long long _den) {
         num = _num/__gcd(_num, _den);
@@ -17,23 +16,20 @@ struct frac {
     void operator/=(frac f) { set_val(num * f.den, den * f.num); }
 };
 
-bool operator<(frac a, frac b) {
-    if((a.den < 0) ^ (b.den < 0)) return a.num * b.den > b.num * a.den;
-    return a.num * b.den < b.num * a.den;
-}
+bool operator==(frac a, frac b) { return a.num * b.den == b.num * a.den; }
+bool operator!=(frac a, frac b) { return !(a == b); }
+bool operator<(frac a, frac b) { return a.num * b.den < b.num * a.den; }
+bool operator<=(frac a, frac b) { return (a == b) || (a < b); }
+bool operator>(frac a, frac b) { return !(a <= b); }
+bool operator>=(frac a, frac b) { return !(a < b); }
+frac operator/(frac a, frac b) { frac ret = a; ret /= b; return ret; }
+frac operator*(frac a, frac b) { frac ret = a; ret *= b; return ret; }
+frac operator+(frac a, frac b) { frac ret = a; ret += b; return ret; }
+frac operator-(frac a, frac b) { frac ret = a; ret -= b; return ret; }
+frac operator-(frac f) { return 0 - f; }}
 
 std::ostream& operator<<(std::ostream& o, const frac f) {
     o << f.num << "/" << f.den;
     return o;
 }
 
-bool operator==(frac a, frac b) { return a.num * b.den == b.num * a.den; }
-bool operator!=(frac a, frac b) { return !(a == b); }
-bool operator<=(frac a, frac b) { return (a == b) || (a < b); }
-bool operator>=(frac a, frac b) { return !(a < b); }
-bool operator>(frac a, frac b) { return !(a <= b); }
-frac operator/(frac a, frac b) { frac ret = a; ret /= b; return ret; }
-frac operator*(frac a, frac b) { frac ret = a; ret *= b; return ret; }
-frac operator+(frac a, frac b) { frac ret = a; ret += b; return ret; }
-frac operator-(frac a, frac b) { frac ret = a; ret -= b; return ret; }
-frac operator-(frac f) { return 0 - f; }
