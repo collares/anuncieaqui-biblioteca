89873996545c74d1ca8d9ce8612c775475464951
Mauricio Collares Neto <mauricioc@gmail.com>
2012-11-11 01:25:09 -0200

    Romberg actually works now (copied from Wikipedia :))

--- math/romberg.cpp
+++ math/romberg.cpp
@@ -1,21 +1,17 @@
 long double romberg(long double a, long double b,
                     long double(*func)(long double)) {
-    long double approx[2][25];
-    long double *cur=approx[1], *prev=approx[0];
+    long double R[16][16], div = (b-a)/2;
 
-    prev[0] = 1/2.0 * (b-a) * (func(a) + func(b));
-    for(int it = 1; it < 25; ++it, swap(cur, prev)) {
-        if(it > 1 && cmp(prev[it-1], prev[it-2]) == 0)
-            return prev[it-1];
-
-        cur[0] = 1/2.0 * prev[0];
-        long double div = (b-a)/pow(2, it);
+    R[0][0] = div * (func(a) + func(b));
+    for(int n = 1; n <= 15; n++, div /= 2) {
+        R[n][0] = R[n-1][0]/2;
         for(long double sample = a + div; sample < b; sample += 2 * div)
-            cur[0] += div * func(a + sample);
-
-        for(int j = 1; j <= it; ++j)
-            cur[j] = cur[j-1] + 1/(pow(4, it) - 1)*(cur[j-1] + prev[j-1]);
+            R[n][0] += div * func(a + sample);
     }
 
-    return prev[24];
+    for(int m = 1; m <= 15; m++)
+        for(int n = m; n <= 15; n++)
+            R[n][m] = R[n][m-1] + 1/(pow(4, m)-1) * (R[n][m-1] - R[n-1][m-1]);
+
+    return R[15][15];
 }
